d6caec22854c74bbeb2b2c3c3c3052b944e8f20f
omerersan <omer.ersan@stu.istinye.edu.tr>
2025-04-03 15:08:32 +0300

    Düzen
    Az toparladım sadece

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart'; // FontAwesome İkonları için gerekli import
+import 'package:quiz_app/screens/search_screen.dart'; // SearchScreen'e yönlendirme için
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+
+    if (index == 1) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => SearchScreen()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null, // AppBar'ı kaldırdık
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            // Kategoriler Başlığı
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Kategoriler',
+                    style:
+                        TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+              ],
+            ),
+            SizedBox(height: 20),
+            // Zaman Butonları
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _buildTimeButton('01:00'),
+                _buildTimeButton('02:00'),
+                _buildTimeButton('03:00'),
+              ],
+            ),
+            SizedBox(height: 20),
+            // Kategoriler Butonları
+            GridView.count(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              shrinkWrap: true,
+              children: <Widget>[
+                _buildCategoryButton(Icons.palette, 'Sanat'),
+                _buildCategoryButton(Icons.calculate, 'Matematik'),
+                _buildCategoryButton(FontAwesomeIcons.paw, 'Hayvanlar'),
+                _buildCategoryButton(Icons.science, 'Bilim'),
+                _buildCategoryButton(FontAwesomeIcons.film, 'Film'),
+                _buildCategoryButton(Icons.gamepad, 'Oyun'),
+              ],
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Zaman butonları
+  Widget _buildTimeButton(String time) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.blue.shade100,
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+      child: Text(time, style: TextStyle(fontSize: 16, color: Colors.white)),
+    );
+  }
+
+  // Kategoriler için butonları oluşturuyoruz
+  Widget _buildCategoryButton(IconData icon, String label) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.blue.shade100,
+        padding: EdgeInsets.all(20),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(icon, size: 40, color: Color(0xFF60A3D9)),
+          SizedBox(height: 10),
+          Text(label, style: TextStyle(fontSize: 16)),
+        ],
+      ),
+    );
+  }
+}
